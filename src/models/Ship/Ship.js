0a58de25333e12b918656c935761935822ceceab
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-11-24 18:09:13 +0700

    feat(Ship): add implementation for Ship class

--- src/models/Ship/Ship.js
+++ src/models/Ship/Ship.js
@@ -1,2 +1,14 @@
 export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits = this.hits + 1;
+  }
+
+  isSunk() {
+    return this.length === this.hits;
+  }
 }
